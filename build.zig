9d426da1416e5e69671d34e3148127efa4de8553
Tim Surber <me@timsurber.de>
2025-09-24 01:26:54 +0200

    convert to zig build system

--- build.zig
+++ build.zig
@@ -21,7 +21,7 @@ const Example = struct {
 
 pub const examples = &[_]Example{
     Example.init("simple", "examples/simple.zig", "A simple hello world app"),
-    Example.init("capi", "examples/capi.zig", "Using the C-api directly"),
+    //Example.init("capi", "examples/capi.zig", "Using the C-api directly"),
     Example.init("customwidget", "examples/customwidget.zig", "Custom widget example"),
     Example.init("image", "examples/image.zig", "Simple image example"),
     Example.init("input", "examples/input.zig", "Simple input example"),
@@ -36,7 +36,7 @@ pub const examples = &[_]Example{
     Example.init("threadawake", "examples/threadawake.zig", "Thread awake example"),
     Example.init("handle", "examples/handle.zig", "Handle example"),
     Example.init("flutterlike", "examples/flutterlike.zig", "Flutter-like example"),
-    Example.init("glwin", "examples/glwin.zig", "OpenGL window example"),
+    // Example.init("glwin", "examples/glwin.zig", "OpenGL window example"),
     Example.init("tile", "examples/tile.zig", "Tile group example"),
 };
 
@@ -58,63 +58,13 @@ pub fn build(b: *std.Build) !void {
         .link_libcpp = true,
     });
 
-    zfltk.linkSystemLibrary("cfltk", .{ .preferred_link_mode = .static });
-    zfltk.linkSystemLibrary("fltk", .{ .preferred_link_mode = .static });
-    zfltk.linkSystemLibrary("fltk_images", .{ .preferred_link_mode = .static });
-    zfltk.linkSystemLibrary("fltk_png", .{ .preferred_link_mode = .static });
-    zfltk.linkSystemLibrary("fltk_jpeg", .{ .preferred_link_mode = .static });
-    zfltk.linkSystemLibrary("fltk_z", .{ .preferred_link_mode = .static });
-    zfltk.linkSystemLibrary("fltk_gl", .{ .preferred_link_mode = .static });
+    const cfltk_pkg = b.dependency("cfltk", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const cfltk_lib = cfltk_pkg.artifact("cfltk");
 
-    const target_os = try std.zig.system.resolveTargetQuery(.{});
-    if (target_os.os.tag == .windows) {
-        zfltk.linkSystemLibrary("ws2_32", .{});
-        zfltk.linkSystemLibrary("comctl32", .{});
-        zfltk.linkSystemLibrary("gdi32", .{});
-        zfltk.linkSystemLibrary("oleaut32", .{});
-        zfltk.linkSystemLibrary("ole32", .{});
-        zfltk.linkSystemLibrary("uuid", .{});
-        zfltk.linkSystemLibrary("shell32", .{});
-        zfltk.linkSystemLibrary("advapi32", .{});
-        zfltk.linkSystemLibrary("comdlg32", .{});
-        zfltk.linkSystemLibrary("winspool", .{});
-        zfltk.linkSystemLibrary("user32", .{});
-        zfltk.linkSystemLibrary("kernel32", .{});
-        zfltk.linkSystemLibrary("odbc32", .{});
-        zfltk.linkSystemLibrary("gdiplus", .{});
-        zfltk.linkSystemLibrary("opengl32", .{});
-        zfltk.linkSystemLibrary("glu32", .{});
-    } else if (target_os.os.tag.isDarwin()) {
-        zfltk.linkFramework("Carbon", .{});
-        zfltk.linkFramework("Cocoa", .{});
-        zfltk.linkFramework("ApplicationServices", .{});
-        zfltk.linkFramework("OpenGL", .{});
-        zfltk.linkFramework("ScreenCaptureKit", .{});
-        zfltk.linkFramework("UniformTypeIdentifiers", .{});
-    } else {
-        zfltk.linkSystemLibrary("wayland-client", .{});
-        zfltk.linkSystemLibrary("wayland-cursor", .{});
-        zfltk.linkSystemLibrary("xkbcommon", .{});
-        zfltk.linkSystemLibrary("dbus-1", .{});
-        zfltk.linkSystemLibrary("EGL", .{});
-        zfltk.linkSystemLibrary("wayland-egl", .{});
-        zfltk.linkSystemLibrary("GL", .{});
-        zfltk.linkSystemLibrary("GLU", .{});
-        zfltk.linkSystemLibrary("pthread", .{});
-        zfltk.linkSystemLibrary("X11", .{});
-        zfltk.linkSystemLibrary("Xext", .{});
-        zfltk.linkSystemLibrary("Xinerama", .{});
-        zfltk.linkSystemLibrary("Xcursor", .{});
-        zfltk.linkSystemLibrary("Xrender", .{});
-        zfltk.linkSystemLibrary("Xfixes", .{});
-        zfltk.linkSystemLibrary("Xft", .{});
-        zfltk.linkSystemLibrary("fontconfig", .{});
-        zfltk.linkSystemLibrary("pango-1.0", .{});
-        zfltk.linkSystemLibrary("pangoxft-1.0", .{});
-        zfltk.linkSystemLibrary("gobject-2.0", .{});
-        zfltk.linkSystemLibrary("cairo", .{});
-        zfltk.linkSystemLibrary("pangocairo-1.0", .{});
-    }
+    zfltk.linkLibrary(cfltk_lib);
 
     if (opts.build_examples) {
         const examples_step = b.step("examples", "build the zfltk examples");
